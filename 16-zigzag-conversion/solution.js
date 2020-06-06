26504506a1cb1305be33dac626db73acf27514d1
Hallie Dunn <hallie.dunn1@gmail.com>
2020-06-06 09:56:13 -0700

    cleaned up solution

--- 16-zigzag-conversion/solution.js
+++ 16-zigzag-conversion/solution.js
@@ -4,14 +4,13 @@ var convert = function(str, numRows) {
     for(let i=0;i<numRows;i++){
         retArr[i] = []
     }
-//     if the length of the string is less than the rows, just return the string cause it isn't going to change
-    if(str.length <= numRows){return str}
+//     if the length of the string is less than the rows, or we only hvae one row, just return the string cause it isn't going to change
+    if(str.length <= numRows || numRows === 1){return str}
 
     let zig = true
     let rowCounter = 0
     for(let i=0;i<str.length;i++){
 
-        cl(zig)
         // add characters into their proper position based on whether we are zigging or zagging
         
         if(rowCounter === 0){
@@ -36,34 +35,13 @@ var convert = function(str, numRows) {
         }
     }
     
-    cl(retArr)
     // at the end, flatten and join return array
-    return retArr.join("")
+    let retStr = ""
+    for(let i=0;i<numRows;i++){
+        retStr += retArr[i].join("")
+    }
+    return retStr
 };
 
-cl(convert("PAYPALISHIRING", 4))
 
-
-function cl(input){
-    console.log(input)
-}
-
-
-// console.log("i-1: ", i-1, "numRows: ", numRows)
-//         if(i+1 === numRows){
-//             // the first time around
-//             cl('ye')
-//             zig = false
-//             rowCounter = retArr.length-2
-//         }
-//         else if(i != 0 && (i+cadenceFixer)%numRows === 0 && zig === true){
-//             // every time this goes around i would need to be checking i, then 1+1, then i+2, and so on, because each time the extra false offsets the cadence 
-//             zig = false
-//             rowCounter = retArr.length-2
-//         }
-//         // i != 0 && i-1%(Math.floor(numRows/2)) === 0 && 
-//         else if(zig === false){
-//             zig = true
-//             rowCounter = 0
-//             cadenceFixer += 1
-//         }
+console.log(convert("PAYPALISHIRING", 4))
