02c7f92077bd52961178e556f2d1be7d6571e50b
hbj <heyhal19@gmail.com>
2020-07-01 17:12:06 -0700

    plus one

---
+++ 22-plus-one/README.md
@@ -0,0 +1,22 @@
+# Plus One
+
+Given a non-empty array of digits representing a non-negative integer, plus one to the integer.
+
+The digits are stored such that the most significant digit is at the head of the list, and each element in the array contain a single digit.
+
+## Examples
+
+```javascript
+    plusOne([1,2,3])
+        => [1,2,4]
+    plusOne([8,9,9,9])
+        => [9,0,0,0]
+    plusOne([1,8,2,9,9])
+        => [1,8,3,0,0]
+```
+
+## Constraints
+- You may assume the integer does not contain any leading zero, except the number 0 itself.
+
+## Source
+[LeetCode](https://leetcode.com/problems/plus-one/)
---
+++ 22-plus-one/solution.js
@@ -0,0 +1,31 @@
+var plusOne = function(digits) {
+    // one option: build the number using math, increment it, deconstruct it using modulo. 
+    // it is true however, that if the input type matches the output type, it's better form not to change the type. 
+    // in order to keep it as an array, you would need to iterate from the back of the array until you found a number that was not 9. if you reach the beginning of the array and all numbers are 9s, that's sort of an edge case, and you will need to return an extra digit at the beginning (a 1) with all other digits being converted to 0. in the normal case, you keep going til you hit something that isnt 9, increment that number, and change the numbers to its right into 0s. 
+        
+    // first edge case: the final element is less than 9. ez.
+        let num = digits.pop()
+        if(num < 9){ return [...digits, num+1]}
+        
+    // count the number of 9s we have (to be re-added as 0s)
+        let counter = 0
+        while(num === 9){
+            counter += 1
+            num = digits.pop()
+        }
+    // if num holds a number, increment it
+        if(num){
+            digits.push(num+1)
+        }
+    // otherwise, just add 1
+        else {
+            digits.push(1)
+        }
+    // add appropriate amount of 0s
+        while(counter > 0){
+            digits.push(0)
+            counter--
+        }
+    //     eh?
+        return digits
+};
