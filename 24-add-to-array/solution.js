a6ea1d40d26ffe3203bc8f03e0684117f7eda59f
hbj <heyhal19@gmail.com>
2020-07-16 14:36:03 -0700

    add to array & remove element

---
+++ 24-add-to-array/solution.js
@@ -0,0 +1,18 @@
+var addToArrayForm = function(A, K) {
+    // build up the num
+    if(!K){return A}
+    let total = 0
+    for(let i=0;i<A.length;i++){
+        // add the int to our total
+        total = total*10+A[i]
+    }
+    total += K
+    A = []
+    // return to array form, unshifting each popped integer
+    while(total>0){
+        let temp = total%10
+        total = Math.floor(total/10)
+        A.unshift(temp)
+    }
+    return A
+};
