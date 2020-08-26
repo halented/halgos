2b09239ebec28ccc1e70aa6026738fd62ce28631
hbj <heyhal19@gmail.com>
2020-08-26 14:39:11 -0700

    oh it works

--- 28-group-size-people-organizer/solution.js
+++ 28-group-size-people-organizer/solution.js
@@ -1,6 +1,3 @@
-// TOTALLY not finished
-
-
 function groupThePeople(sizes) {
     let ans = []
     let len = sizes.length
@@ -23,10 +20,10 @@ function groupThePeople(sizes) {
             ans.push([i])
             tracking[i] = "used"
         }
-        else {
-            // otherwise, we know that there are at least currentSize number of this currentSize in the sizes array, and we must make an entry into the answers array and fill it with the right elements. we need a way of making sure that each size is accounted for once and only once
+        else if(tracking[i]=== "unused"){
+            // otherwise, we know that there are at least currentSize number of people in the sizes array who have this size, and we must make an entry into the answers array and fill it with the right elements. we need a way of making sure that each size is accounted for once and only once
             let temp = [i]
-            for (let j = i; j < len; j++) {
+            for (let j = i+1; j < len; j++) {
                 // IF the size is right, the array isn't full yet, and the person hasn't already been assigned
                 if(sizes[j] === currentSize && temp.length<currentSize && tracking[j] === "unused"){
                     temp.push(j)
@@ -38,3 +35,8 @@ function groupThePeople(sizes) {
     }
     return ans
 }
+
+console.log(groupThePeople([2,1,3,3,3,2]))
+
+// groupThePeople([3,3,3,3,3,1,3])
+// => [[5],[0,1,2],[3,4,6]]
