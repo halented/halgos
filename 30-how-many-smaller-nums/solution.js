6be85f22dfbd5ccf1d89945a13572461bf84aa3e
hbj <heyhal19@gmail.com>
2020-08-29 00:38:11 -0700

    hmmm not great

--- 30-how-many-smaller-nums/solution.js
+++ 30-how-many-smaller-nums/solution.js
@@ -1,23 +1,24 @@
-var smallerNumsThanCurrent = function (nums) {
-    // return an array, and in place of each num, instead report the amount of other numbers in the array which are smaller than it.
-    let counts = []
-    // brute force: iterate through nums. for each num, iterate through the rest of them and build a count, which is dropped into the counts array.
-    for (let i = 0; i < nums.length; i++) {
-        let current = nums[i]
-        let tempCount = 0
+// var smallerNumsThanCurrent = function (nums) {
+//     // return an array, and in place of each num, instead report the amount of other numbers in the array which are smaller than it.
+//     let counts = []
+//     // brute force: iterate through nums. for each num, iterate through the rest of them and build a count, which is dropped into the counts array.
+//     for (let i = 0; i < nums.length; i++) {
+//         let current = nums[i]
+//         let tempCount = 0
 
-        for (let j = 0; j < nums.length; j++) {
-            if (nums[j] < current) {
-                tempCount += 1
-            }
-        }
+//         for (let j = 0; j < nums.length; j++) {
+//             if (nums[j] < current) {
+//                 tempCount += 1
+//             }
+//         }
 
-        counts.push(tempCount)
-    }
+//         counts.push(tempCount)
+//     }
 
-    return counts
-}
+//     return counts
+// }
 
+// a fundamental problem with the approach below: hash keys are unique
 function alt(nums){
     // let indexSaver = {8:0, 1:1}
     // let countSaver = {8:1, 1:0}
@@ -31,8 +32,16 @@ function alt(nums){
     nums.sort()
     for(let i=0;i<nums.length;i++){
         let currentNum = nums[i]
-        countSaver[currentNum] = i
+        // if we have duplicates, keep iterating til we find something greater than the currentNum
+        let j = i + 1
+        while(nums[j] === nums[i]){
+            j++
+        }
+        countSaver[currentNum] += 1
     }
+    console.log(countSaver)
 }
+
+console.log(alt([8,1,2,2,3]))
 
 
