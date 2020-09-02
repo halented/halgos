4208715d698c459118a45b57b9cffe872c10be3f
hbj <heyhal19@gmail.com>
2020-09-02 11:24:56 -0700

    setup

--- 30-how-many-smaller-nums/solution.js
+++ 30-how-many-smaller-nums/solution.js
@@ -1,22 +1,23 @@
-// var smallerNumsThanCurrent = function (nums) {
-//     // return an array, and in place of each num, instead report the amount of other numbers in the array which are smaller than it.
-//     let counts = []
-//     // brute force: iterate through nums. for each num, iterate through the rest of them and build a count, which is dropped into the counts array.
-//     for (let i = 0; i < nums.length; i++) {
-//         let current = nums[i]
-//         let tempCount = 0
+var smallerNumsThanCurrent = function (nums) {
+    // return an array, and in place of each num, instead report the amount of other numbers in the array which are smaller than it.
+    let counts = []
+    // brute force: iterate through nums. for each num, iterate through the rest of them and build a count, which is dropped into the counts array.
+    for (let i = 0; i < nums.length; i++) {
+        let current = nums[i]
+        let tempCount = 0
 
-//         for (let j = 0; j < nums.length; j++) {
-//             if (nums[j] < current) {
-//                 tempCount += 1
-//             }
-//         }
+        for (let j = 0; j < nums.length; j++) {
+            if (nums[j] < current) {
+                tempCount += 1
+            }
+        }
 
-//         counts.push(tempCount)
-//     }
+        counts.push(tempCount)
+    }
 
-//     return counts
-// }
+    return counts
+}
+// above solution is o(n)2
 
 // a fundamental problem with the approach below: hash keys are unique
 function alt(nums){
---
+++ 31-split-balanced-string/README.md
@@ -0,0 +1,23 @@
+# Split a String in Balanced String
+
+"Balanced" strings are those who have equal quantity of 'L' and 'R' characters.
+
+Given a balanced string `s` split it in the maximum amount of balanced strings.
+
+Return the maximum amount of splitted balanced strings.
+
+## Examples
+
+```javascript
+    balancedStringSplit("RLRRLLRLRL")
+        => 4
+    balancedStringSplit("RLLLLRRRLR")
+        => 3
+```
+
+## Constraints
+- `1 <= s.length <= 1000`
+- `s[i] = 'L' or 'R'`
+
+## Source
+[LeetCode](https://leetcode.com/problems/split-a-string-in-balanced-strings/)
