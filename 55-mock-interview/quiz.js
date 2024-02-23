6927aa51a8e3207ba2d1cafe8d3f914f7218e5a4
Hal Friday <heyhal19@gmail.com>
2024-02-22 17:40:19 -0800

    generate nums better

--- 55-mock-interview/quiz.js
+++ 55-mock-interview/quiz.js
@@ -25,21 +25,40 @@ const input = JSON.stringify({
     ]
 });
 
+// generate x nums between 0 and y which are not the same as each other and not 1
+/**
+ * 
+ * @param {number} x - number of numbers to generate
+ * @param {number} y - upper boundary for valid numbers to generate
+ * @param {number} z - forbidden number
+ * @returns array of numbers which do not repeat and are not z
+ */
+const generateNums = (x, y, z) => {
+    let nums = {};
+    let i = 0;
+    while (i < x) {
+        // generate a number
+        let num = Math.floor(Math.random() * y)
+        // if it already exists in the hash or is 1, repeat step 1
+        while (nums[num] || num === z) {
+            num = Math.floor(Math.random() * y)
+        }
+        // enter it into the hash
+        nums[num] = true;
+        i++;
+    };
+    return Object.keys(nums);
+}
+
 const quizGenerator = (json) => {
     // return obj format: {question: string, options: string[], answer: string}
     let parsed = JSON.parse(json).data
     let output = parsed.map((w, i) => {
         let quizItem = { "question": w.en, "options": [w.es], "answer": w.es }
-        // this part ugly as hell lol
-        let j = Math.floor(Math.random() * parsed.length);
-        while (j === i) {
-            j = Math.floor(Math.random() * parsed.length);
-        }
-        let k = i;
-        while (k === i || k === j) {
-            k = Math.floor(Math.random() * parsed.length);
-        }
-        quizItem.options.push(parsed[k].es, parsed[j].es)
+
+        const optionIndices = generateNums(2, parsed.length, i)
+        optionIndices.forEach(j => quizItem.options.push(parsed[j].es))
+
         return quizItem
     })
     return JSON.stringify({ "data": output })
@@ -57,3 +76,4 @@ console.log(quizGenerator(input));
  * 2. pick two random numbers using Math.random, grab the es words and toss em in the options array
  *      => kinda like this, it's super simple and not replicable. but, it wouldn't guarantee that all questions receive like the same options. as a first pass let's just do that
  */
+
